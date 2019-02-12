fa796ba8ffdee9b5102849bead2b1e2bbed04a18
Aaron Kable <aaronkable@gmail.com>
2019-02-12 01:35:10 -0500

    BASIC error handling ( fk errors )

--- helpersignals/signals.py
+++ helpersignals/signals.py
@@ -10,17 +10,20 @@ import json
 def new_app(sender, instance, created, **kwargs):
     if created:
         print("New Application! %s" % instance.main_character , flush=True)
-        url = "Link to auth"
-        main_char = instance.main_character
-        corp = instance.form.corp
-        embed_list = [{'title':"New Application", 'description':("From %s on auth"%main_char.character_name), 'image':{'url': ('https://imageserver.eveonline.com/Character/%s_128.jpg' % (main_char.character_id))}}]
-        hooks = ApplicationWebhook.objects.filter(corp=corp)
+        try:
+            url = "Link to auth"
+            main_char = instance.main_character
+            corp = instance.form.corp
+            embed_list = [{'title':"New Application", 'description':("From %s on auth"%main_char.character_name), 'image':{'url': ('https://imageserver.eveonline.com/Character/%s_128.jpg' % (main_char.character_id))}}]
+            hooks = ApplicationWebhook.objects.filter(corp=corp)
         
-        for hook in hooks:
-            if hook.enabled:
-                custom_headers = {'Content-Type': 'application/json'}
-                alertText = ""
+            for hook in hooks:
+                if hook.enabled:
+                    custom_headers = {'Content-Type': 'application/json'}
+                    alertText = ""
 
-                r = requests.post(hook.webhook, headers=custom_headers, data=json.dumps({'content':alertText, 'embeds': embed_list}))
-            #logger.error("Got status code %s after sending ping" % r.status_code)
-                r.raise_for_status()
+                    r = requests.post(hook.webhook, headers=custom_headers, data=json.dumps({'content':alertText, 'embeds': embed_list}))
+                #logger.error("Got status code %s after sending ping" % r.status_code)
+                    r.raise_for_status()
+        except:
+            pass # shits fucked... 
